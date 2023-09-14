37fef5a86e71c4b773e21d494fac7bfeb434742a
ajdlsk123 <pou5584@naver.com>
2023-09-14 14:16:03 +0900

    SSW

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import max_profit as mp
 import prime_number as pn
 
 if __name__ == '__main__':
-    pn.is_prime_number(1)
-    pn.is_prime_number(13)
-    pn.is_prime_number(24)
-    pn.is_prime_number(31)
+    pn.is_prime_number(5)
+    pn.is_prime_number(21)
+    pn.is_prime_number(28)
+    pn.is_prime_number(45)
     
